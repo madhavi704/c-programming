9d7dfed71ba32dbf5ffc7fa5d8b6ae9748ce76b5
madhavi704 <madhavidonkada147@gmail.com>
2025-11-12 21:15:35 +0530

    Add files via upload

---
+++ simple if.cpp
@@ -0,0 +1,14 @@
+// write a c program simple if 
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("enter a number:");
+	scanf("%d,&a");
+	if(a>100)
+	{
+		printf("a is greater than 100\n");
+	}
+	printf("end");
+	return 0;
+}
