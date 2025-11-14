2e47688febfc480f9785595719bd87bd0a259550
madhavi704 <madhavidonkada147@gmail.com>
2025-11-14 19:14:03 +0530

    Add files via upload

---
+++ area and perimeter.cpp
@@ -0,0 +1,12 @@
+// write a c program to find square root of a given number
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int n;
+	printf("enter a number:");
+	scanf("%d,&n");
+	printf("sqrt(%d)=%f\n",sqrt(n));
+	return 0;
+}
+
